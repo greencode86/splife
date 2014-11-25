2af2f1cc68a68d662c62dbec24e7380925f6eb05
Ezequiel Lamonica <lamonicaezequiel@gmail.com>
2014-11-25 16:30:15 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brightmediums::Application.config.secret_key_base = 'cefa4522d33423fe989ad3647ede67a405cb9fdf3b0b317fe3b33d5b4013846bb03dcb17f45599daf11419a79fbad4184e0c74c57c497bcf3cb699dfba818328'
